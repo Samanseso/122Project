32a86b7f1658ae807c6c1b6671051348cb42e8d7
students <students@labmain.arellano.edu.ph>
2024-03-25 17:14:29 +0800

    search method added

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+function myFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.querySelector('#search_input');
+    filter = input.value.toUpperCase();
+    ul = document.querySelector(".search_items");
+    li = ul.getElementsByTagName('li');
+
+    if(filter.length > 0) {
+        ul.style.display = "block";
+        for (i = 0; i < li.length; i++) {
+            a = li[i].getElementsByTagName("a")[0];
+            txtValue = a.textContent || a.innerText;
+    
+    
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+    }
+    else {
+        ul.style.display = "none";
+    }
+
+    // Loop through all list items, and hide those who don't match the search query
+    
+  }
+
 //Menu bar overlaying
 function openNav() {
     document.getElementById("mySidenav").style.left= "30vw";
